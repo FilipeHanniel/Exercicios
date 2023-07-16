0827468bdf8bf6c4bdd48d788aba251670830e5d
Filipe Hanniel <filipehanniel@gmail.com>
2023-07-16 17:41:53 -0300

    exercicio6

--- src/exerc06.java
+++ src/exerc06.java
@@ -7,6 +7,9 @@ public class exerc06 {
         float valor;
         float somaValor = 0;
         int cont = 0;
+        int ateMedia = 0;
+        int acimaMedia = 0;
+        float media = 0;
 
         Scanner sc = new Scanner(System.in);
 
@@ -20,11 +23,19 @@ public class exerc06 {
             if (valor != 0) {
                 somaValor = somaValor + valor;
                 cont += 1;
+                media = somaValor / cont;
+                if (valor <= media) {
+                    ateMedia += 1;
+                } else {
+                    acimaMedia += 1;
+                }
             }
            
         } while (valor != 0);
         
-        System.out.println("A média dos salários digitados é: " + somaValor / cont);
+        System.out.println("A média dos salários digitados é: " + media);
+        System.out.println("A quantidade de salários até a média são: " + ateMedia);
+        System.out.println("A quantidade de salarios acima da media são: " + acimaMedia);
 
         sc.close();
 
