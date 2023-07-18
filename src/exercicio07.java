63abeab2fdbfe4ebc72fa46ac640b02acb944b47
Filipe Hanniel <filipehanniel@gmail.com>
2023-07-18 20:01:00 -0300

    exercicios 7

--- src/exercicio07.java
+++ src/exercicio07.java
@@ -3,35 +3,39 @@ import java.util.Scanner;
 public class exercicio07 {
 
     public static void main(String[] args) {
-        
-        
+
         float valor;
         char sexo;
         int idade;
-        int contMasc = 0; //Quantidade de eleitores masculinos.
+        int contMasc = 0; // Quantidade de eleitores masculinos.
         int contFem = 0;
-        float idadeMasc = 0; //Soma das idades dos eleitores masculinos.
+        float idadeMasc = 0; // Soma das idades dos eleitores masculinos.
         float idadeFem = 0;
         int votos10 = 0;
         int votos20 = 0;
         int votos30 = 0;
         int votos40 = 0;
         int votos50 = 0;
-        int h10 =0, h20 = 0, h30 = 0, h40 = 0,  h50 = 0, m10 = 0, m20 = 0, m30 = 10, m40 = 10, m50 = 10; //acumulador da quantidade de eleitor m\f de cada candidato
+        int h10 = 0, h20 = 0, h30 = 0, h40 = 0, h50 = 0, m10 = 0, m20 = 0, m30 = 10, m40 = 10, m50 = 10; // acumulador
+                                                                                                         // da
+                                                                                                         // quantidade
+                                                                                                         // de eleitor
+                                                                                                         // m\f de cada
+                                                                                                         // candidato
         int hvencedor = 0, mvencedor = 0;
-        int idadevencedor = 0; //soma das idades dos eleitores do vencedor.
-        int votosvencedor = 0; //soma dos votos do vencedor.
-        int idade10 = 0, idade20 = 0, idade30 = 0, idade40 = 0, idade50 = 0; //acumulador das idades dos eleitores de cada canditado.
+        int idadevencedor = 0; // soma das idades dos eleitores do vencedor.
+        int votosvencedor = 0; // soma dos votos do vencedor.
+        int idade10 = 0, idade20 = 0, idade30 = 0, idade40 = 0, idade50 = 0; // acumulador das idades dos eleitores de
+                                                                             // cada canditado.
         String vencedor;
-        int cod; //codigo de escolha para realizar o calculo das letras "f" e "g" do exercício.
+        int cod; // codigo de escolha para realizar o calculo das letras "f" e "g" do exercício.
 
-       
         Scanner sc = new Scanner(System.in);
 
         System.out.println(" === PESQUISA ELEITORAL GOIÂNIA ===");
         System.out.println("Digite ZERO para sair.");
         System.out.println("Digite os dados, Nº DO CANDIDATO, SEXO e IDADE DO ELEITOR.");
-                
+
         do {
             do {
                 System.out.print("Digite o número do candidato: ");
@@ -46,9 +50,9 @@ public class exercicio07 {
                 sexo = sc.next().charAt(0);
                 System.out.println("Digite a idade do eleitor: ");
                 idade = sc.nextInt();
-                
+
                 if (sexo == 'm') {
-                    contMasc += 1; 
+                    contMasc += 1;
                     idadeMasc = idadeMasc + idade;
                 } else {
                     contFem += 1;
@@ -96,20 +100,20 @@ public class exercicio07 {
                         m50 += 1;
                     }
                 }
-                
+
             }
-           
+
         } while (valor != 0);
 
         if (votos10 > votos20 & votos10 > votos30 & votos10 > votos40 & votos10 > votos50) {
             vencedor = "Candidato 10";
             cod = 1;
-        } else  if (votos20 > votos10 & votos20 > votos30 & votos20 > votos40 & votos20 > votos50) { 
+        } else if (votos20 > votos10 & votos20 > votos30 & votos20 > votos40 & votos20 > votos50) {
             vencedor = "Candidato 20";
             cod = 2;
         } else if (votos30 > votos10 & votos30 > votos20 & votos30 > votos40 & votos30 > votos50) {
             vencedor = "Candidato 30";
-            cod = 3;            
+            cod = 3;
         } else if (votos40 > votos10 & votos40 > votos20 & votos40 > votos30 & votos40 > votos50) {
             vencedor = "Candidato 40";
             cod = 4;
@@ -149,21 +153,20 @@ public class exercicio07 {
                 idadevencedor = idade50;
                 votosvencedor = votos50;
                 break;
-            
+
         }
-        
+
         System.out.println("Quantidade de eleitores masculinos: " + contMasc);
         System.out.println("Quantidade de eleitores femininos: " + contFem);
         System.out.println("Idade média dos eleitores masculinos é: " + idadeMasc / contMasc + " anos.");
-        System.out.println("Idade média dos eleitores femininos é: " + idadeFem / contFem + " anos.");        
+        System.out.println("Idade média dos eleitores femininos é: " + idadeFem / contFem + " anos.");
         System.out.println("O vencedor da eleição é o " + vencedor);
         System.out.println("Os votantes do candidato vencedor do sexo masculino somam um total de " + hvencedor);
         System.out.println("Os votantes do candidato vencedor do sexo feminino somam um total de " + mvencedor);
-        System.out.println("A idade média dos eleitores do vencedor é de " + idadevencedor / votosvencedor );        
-        
+        System.out.println("A idade média dos eleitores do vencedor é de " + idadevencedor / votosvencedor);
 
         sc.close();
 
     }
-    
+
 }
