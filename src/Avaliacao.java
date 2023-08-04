08dce2730875df909721fbfcee54f9cd9b74313c
Filipe Hanniel <filipehanniel@gmail.com>
2023-08-03 21:59:44 -0300

    aula final

--- src/Avaliacao.java
+++ src/Avaliacao.java
@@ -9,12 +9,12 @@ public class Avaliacao {
         double temperaturaC, temperaturaF;
 
         System.out.println("      --- Conversor de Cº --> Fº ---");
-
+        
         do {
             System.out.print("Informe a temperatura em graus Celsius ou zero para sair: ");
             temperaturaC = Double.parseDouble(sc.nextLine());
             if (temperaturaC != 0) {
-                temperaturaF = (temperaturaC * (9/5) + 32);
+                temperaturaF = (temperaturaC * 9/5 + 32);
                 System.out.println(temperaturaC + "ºC corresponde a " + temperaturaF + "ºF graus Fahrenheit.");
             }
         } while (temperaturaC != 0);
